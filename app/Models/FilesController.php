6eba9b40806e8183c85ad2763fdf878246b4e5ac
LARISSA <tientcheularissaleina@gmail.com>
2023-12-22 09:31:08 +0100

    first commit

---
+++ app/Models/FilesController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilesController extends Controller
+{
+    public function hello(){
+    return view ('hey');
+    } 
+    
+}
