78e3f030f81bc42f5c544be34821f218aa0475b0
LARISSA <tientcheularissaleina@gmail.com>
2024-02-12 16:52:50 +0100

    Layouts of frontend

---
+++ resources/views/layouts/frontend/header.blade.php
@@ -0,0 +1,97 @@
+<header class="header__area-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-xxl-12">
+          <div class="header__inner">
+            <div class="header__left">
+              <div class="header__logo">
+                <a href="">
+                  <img src="{{asset('assets/imgs/home-1/loko.png')}}" alt="Logo">
+                </a>
+              </div>
+            </div>
+            <div class="header__right">
+              <div class="header__btm">
+                <div class="header__menu">
+                  <div class="main-menu-2">
+                    <ul>
+                      <li class="has-dropdown"><a href="#">Dashboard</a>
+                        <ul class="main-dropdown">
+                          <li><a href="index.html">Dashboard</a></li>
+                        </ul>
+                      </li>
+                      <li class="has-dropdown"><a href="#">courses</a>
+                        <ul class="main-dropdown">
+                          <li><a href="course.html">Course</a></li>
+                          <li><a href="course-2.html">Course v2</a></li>
+                          <li><a href="course-details.html">Course Details</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="about.html">About</a>
+                      </li>
+                      <li class="has-dropdown"><a href="#">pages</a>
+                        <ul class="main-dropdown">
+                          <li><a href="course.html">Course</a></li>
+                          <li><a href="course-2.html">Course v2</a></li>
+                          <li><a href="course-details.html">Course Details</a></li>
+                          <li><a href="blog.html">Blog</a></li>
+                          <li><a href="blog-listing.html">Blog v.2</a></li>
+                          <li><a href="blog-details.html">Blog Details</a></li>
+                          <li><a href="shop.html">Shop</a></li>
+                          <li><a href="product-details.html">Shop Details</a></li>
+                          <li><a href="team.html">team</a></li>
+                          <li><a href="faq.html">Faq</a></li>
+                          <li><a href="contact.html">contact</a></li>
+                          <li><a href="gallary.html">Gallary</a></li>
+                          <li><a href="404.html">404</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="{{route('login')}}">login</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="header__other">
+                  <div class="header__lang header__lang-2">
+                    <div class="dropdown">
+                      <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="true"><i class="fa-solid fa-globe"></i>Eng</button>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Eng</a></li>
+                        <li><a class="dropdown-item" href="#">France</a></li>
+                        <li><a class="dropdown-item" href="#">spanish</a></li>
+                        <li><a class="dropdown-item" href="#">German</a></li>
+                        <li><a class="dropdown-item" href="#">Poland</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="header__cart header__cart-2">
+                    <button class="cart" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                      <i class="fa-solid fa-cart-shopping"></i>
+                    </button>
+                  </div>
+                  <div class="header__search">
+                    <button class="search-icon"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    <button class="search-close"><i class="fa-solid fa-xmark"></i></button>
+                  </div>
+                  <div class="header__btn">
+                    <a href="#" class="btn-ractangle">Join Now</a>
+                  </div>
+                  <div class="mobile__menu">
+                    <button class="menu-icon"><i class="fa-solid fa-bars"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container position-relative">
+      <div class="search__form">
+        <form action="#">
+          <input type="text" name="s" placeholder="Search...">
+        </form>
+      </div>
+    </div>
+  </header>
---
+++ resources/views/layouts/frontend/main.blade.php
@@ -0,0 +1,154 @@
+
+</html><!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="HTML5 Template">
+
+  <!-- Site Title -->
+  <title>@yield('title')</title>
+
+  <!-- Site Favicon -->
+  <link rel="shortcut icon" href="{{asset('assets/imgs/logo/favicon.png')}}" type="image/x-icon">
+
+  <!-- All CSS Files -->
+  <link rel="stylesheet" href="{{asset("assets/css/all.min.css")}}">
+  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/meanmenu.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/magnifi.css') }}">
+  <link rel="stylesheet" href="assets/css/aos.css">
+  <link rel="stylesheet" href="{{ asset('assets/css/master.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+<style>
+    .hover-class:hover{
+        color:  white;
+        background-color: black;
+    }
+</style>
+</head>
+
+<body>
+
+  <!-- Preloader -->
+  <div id="preloader">
+    <div id="loader"></div>
+  </div>
+
+
+  <!-- Sroll to top -->
+  <div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+      <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"></path>
+    </svg>
+  </div>
+
+
+  <!-- Offcanvas area start -->
+  <div class="offcanvas__area">
+    <div class="offcanvas__inner">
+      <div class="offcanvas__top">
+        <img src="{{asset('assets/imgs/home-1/loko.png')}}" alt="Logo" height="5" width="5">
+        <button id="offcanvas_close"><i class="fa-solid fa-xmark"></i></button>
+      </div>
+      <div class="offcanvas__menu">
+        <div class="offcanvas-menu">
+          <ul>
+            <li><a href="index.html">Dashboard</a>
+            </li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="index.html">Service</a>
+            </li>
+            <li><a href="#">portfolio</a>
+            </li>
+            <li><a href="#">team</a>
+            </li>
+            <li><a href="#">blog</a>
+            </li>
+            <li><a href="#">others</a>
+            </li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Offcanvas area end -->
+
+
+  <!-- Cart area start -->
+  <div class="offcanvas offcanvas-end cart__offcanvas" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasRightLabel">SHOPPING CART</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <div class="cart__list">
+        <div class="cart__item">
+          <div class="image">
+            <a href="">
+              <img src="{{ asset('assets/imgs/home-1/rw-2.png') }}" alt="Image">
+            </a>
+          </div>
+          <div class="info">
+            <a href="">
+              <h3 class="title">Education Geometry New Ranking Strong Year-2023</h3>
+            </a>
+            <p>1x$500</p>
+          </div>
+          <button class="delete">X</button>
+        </div>
+        <div class="cart__item">
+          <div class="image">
+            <a href="">
+              <img src="{{ asset('assets/imgs/home-1/rw.png') }}" alt="Image">
+            </a>
+          </div>
+          <div class="info">
+            <a href="">
+              <h3 class="title">Education Geometry New Ranking Strong Year-2023</h3>
+            </a>
+            <p>1x$500</p>
+          </div>
+          <button class="delete">X</button>
+        </div>
+      </div>
+
+      <div class="cart__other">
+        <p>SUBTOTAL:  <span>$121.00</span></p>
+        <a href="">view cart</a>
+        <a href="">checkout</a>
+      </div>
+    </div>
+  </div>
+  <!-- Cart area end -->
+
+
+
+  <!-- Header area start -->
+  @include('layouts.frontend.header')
+  <!-- Header area end -->
+<!-- Footer area start  -->
+@yield('content')
+@include('layouts.frontend.footer')
+  <!-- Footer area end  -->
+
+
+
+  <!-- All JS Files -->
+  <script src="assets/js/jquery-3.6.1.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/jquery.meanmenu.min.js"></script>
+  <script src="assets/js/swiper-bundle.min.js"></script>
+  <script src="assets/js/mixitup.min.js"></script>
+  <script src="assets/js/counter.js"></script>
+  <script src="assets/js/aos.js"></script>
+  <script src="assets/js/backToTop.js"></script>
+  <script src="assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
